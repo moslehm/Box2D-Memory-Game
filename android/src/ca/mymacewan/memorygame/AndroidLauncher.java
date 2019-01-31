c3c19767f635b26b8f00608c1b02c7ae8a373efb
Mohammad <moslehm@mymacewan.ca>
2019-01-31 00:05:00 -0700

    Add newly generated libGDX files as the base for the game.
    This project was generated to work with IntelliJ, other IDE could be used, but the project has to be exported which can cause merge issues. The existing java file has not been modified. Additional java classes of any kind should be placed in ~\w19Wozniak\core\src\ca\mymacewan\memorygame

---
+++ android/src/ca/mymacewan/memorygame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ca.mymacewan.memorygame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ca.mymacewan.memorygame.MemoryGameView;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MemoryGameView(), config);
+	}
+}
---
+++ desktop/src/ca/mymacewan/memorygame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ca.mymacewan.memorygame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ca.mymacewan.memorygame.MemoryGameView;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MemoryGameView(), config);
+	}
+}
