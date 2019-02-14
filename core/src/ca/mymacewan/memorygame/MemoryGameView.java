02698fcaa7113dac04ffdf63e1023dfdcaaeb1a6
moslehm <32352647+moslehm@users.noreply.github.com>
2019-02-14 13:08:51 -0700

    Update back end game logic to work with the game view
    Most of the changes here were made by Jonathan to make the code testable
    These are temporary changes that will have to be revisited to make the
    back end communicate with the view efficiently.

--- core/src/ca/mymacewan/memorygame/MemoryGameView.java
+++ core/src/ca/mymacewan/memorygame/MemoryGameView.java
@@ -4,30 +4,146 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.*;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.actions.RunnableAction;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
+import com.badlogic.gdx.utils.Align;
+
+import java.util.ArrayList;
+
+import static java.lang.Math.sqrt;
 
 public class MemoryGameView extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+    MemoryGame game;
+	Stage stage;
+    int gameSize = 16;
+
+	public class CardGroup extends Group {
+		Texture backTexture, frontTexture;
+		Image displayedImage;
+
+		final Card card;
+
+		public CardGroup(final Card card, int x, int y){
+			backTexture = Assets.manager.get(Assets.cardBack);
+			frontTexture = Assets.manager.get(Assets.demoCard + card.getValue() + ".png");
+			displayedImage = new Image(backTexture);
+
+			float[] convertedXY = convertXY(x, y, (int) (gameSize/sqrt(gameSize)));
+
+			displayedImage.setPosition(convertedXY[0], convertedXY[1]);
+			displayedImage.setOrigin(Align.center);
+			this.addActor(displayedImage);
+
+			setOrigin(Align.center);
+
+			this.card = card;
+			setUserObject(this.card);
+
+			addListener((new ClickListener(){
+				public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+					if (game.isLegalMove((Card)getUserObject())) {
+						RunnableAction changeTexture = new RunnableAction() {
+							@Override
+							public void run() {
+								displayedImage.setDrawable(new SpriteDrawable(new Sprite(frontTexture)));
+							}
+						};
+						displayedImage.addAction(Actions.sequence(
+								MyActions.flipOut(0.1f),
+								changeTexture,
+								MyActions.flipIn(0.1f)));
+
+						((Card)getUserObject()).setID(pointer);
+						((Card)getUserObject()).setState(State.REVEALED);
+					}
+					return true;
+				}
+				public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
+                    if (((Card)getUserObject()).getState()!= State.PAIRED && ((Card)getUserObject()).getID() == pointer){
+						RunnableAction changeTexture = new RunnableAction() {
+							@Override
+							public void run() {
+								displayedImage.setDrawable(new SpriteDrawable(new Sprite(backTexture)));
+							}
+						};
+						displayedImage.addAction(Actions.sequence(
+								MyActions.flipOut(0.1f),
+								changeTexture,
+								MyActions.flipIn(0.1f)));
+						//if ()
+						((Card) getUserObject()).setState(State.HIDDEN);
+					}
+				}
+			}));
+		}
+
+		private float[] convertXY(int x, int y, int cardsPerRow){
+			float xOffset = Gdx.graphics.getWidth() - (cardsPerRow * 150f + (float)(10 * (cardsPerRow - 1)));
+			float yOffset = Gdx.graphics.getHeight() - (cardsPerRow * 150f + (float)(10  * (cardsPerRow - 1)));
+			float[] convertedXY = {0f, 0f};
+
+			convertedXY[0] = xOffset/2f + ((160f * ((float) x)));
+			convertedXY[1] = yOffset/2f + ((160f * ((float) y)));
+
+			return convertedXY;
+		}
+
+		@Override
+		public void draw(Batch batch, float alpha) {
+				displayedImage.draw(batch, alpha);
+		}
+	}
+
 	@Override
 	public void create () {
+    	Assets.load();
+    	Assets.manager.finishLoading();
+
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
+
+        game = new MemoryGame();
+        game.numOfCards = gameSize;
+        game.gameStart();
+
+        int gameRows = (int)(gameSize/sqrt(gameSize));
+        ArrayList<Card> cards = game.getCards();
+		for (int x = 0; x < gameRows; x++){
+			for (int y = 0; y < gameRows; y++){
+				Card currentCard = cards.get((x * gameRows) + y);
+				CardGroup newCardGroup = new CardGroup(currentCard, x, y);
+
+				stage.addActor(newCardGroup);
+			}
+		}
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(44/255f, 135/255f, 209/255f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
+
 		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		Assets.dispose();
+		stage.dispose();
 	}
 }
--- desktop/src/ca/mymacewan/memorygame/desktop/DesktopLauncher.java
+++ desktop/src/ca/mymacewan/memorygame/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import ca.mymacewan.memorygame.MemoryGameView;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1366;
+		config.height = 768;
 		new LwjglApplication(new MemoryGameView(), config);
 	}
 }
