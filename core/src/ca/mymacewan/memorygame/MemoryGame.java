7d50dc639206a3eee88f9ff7b2920ee8178776f3
Mohammad <moslehm@mymacewan.ca>
2019-03-03 01:32:44 -0700

    Make game logic work with multiple cards held down
    Also cleaned up code

--- core/src/ca/mymacewan/memorygame/MemoryGame.java
+++ core/src/ca/mymacewan/memorygame/MemoryGame.java
@@ -9,36 +9,50 @@ import static ca.mymacewan.memorygame.State.PAIRED;
 import static ca.mymacewan.memorygame.State.REVEALED;
 
 public class MemoryGame {
-    public int numOfCards;// How many cards in the game. it be increased when difficulty increase.
-    private ArrayList<Card> Cards = new ArrayList<Card>(numOfCards);
+    int numOfCards; // How many cards in the game. it be increased when difficulty increase.
+    private ArrayList<Card> cards = new ArrayList<Card>(numOfCards);
 
-    public void gameStart(){
-        for (int i = 0 ; i < numOfCards; i++){
+    /**
+     * Starts the game after numOfCards had been set
+     */
+    void gameStart(){
+        for (int i = 0 ; i < numOfCards; i++) {
             Card tempCard = new Card();
+
             int cardValue = i/2;
             tempCard.setValue(Integer.toString(cardValue));
-            //tempCard.setID(i);
-            Cards.add(tempCard);
+            cards.add(tempCard);
         }
-        Collections.shuffle(Cards);
+        Collections.shuffle(cards);
     }
 
- /*   public MemoryGame() {
-
-        // Implement dealing + shuffle in here.
-
-    }*/
-
-    public void makeMove(int index) {
-        if (!(isLegalMove(Cards.get(index)))) {
+    /**
+     * Sets the card at index to REVEALED if isLegalMove returns true
+     * @param index of card in cards
+     */
+    void flipUp(int index) {
+        if (!(isLegalMove(cards.get(index)))) {
             return;
         }
-        Iterator<Card> itr = Cards.iterator();
+
+        Card card = cards.get(index);
+        card.setState(REVEALED);
+        for (Card currentCard : cards) {
+            if (!currentCard.equals(card) && Integer.parseInt(currentCard.getValue()) == Integer.parseInt(card.getValue())) {
+                if (currentCard.getState() == REVEALED) {
+                    currentCard.setState(PAIRED);
+                    card.setState(PAIRED);
+                    return;
+                }
+            }
+        }
+        // Commented out because the code above has the same functionality but for multiple cards
+        /*Iterator<Card> itr = cards.iterator();
         while (itr.hasNext()) {
             Card i = itr.next();
             // FOR IF ARRAY HAS A CARD ALREADY REVEALED. HANDLES PAIRING AND MISMATCHES
             if (i.getState() == REVEALED) {
-                Card j = Cards.get(index);
+                Card j = cards.get(index);
                 if (j.getState() != REVEALED) {
                     j.setState(REVEALED);
                     if (i.getValue().equals(j.getValue())) {
@@ -54,18 +68,32 @@ public class MemoryGame {
                 }
             }
         }
-        // FOR IF ARRAY HAS NO REVEALED CARDS
-        Card i = Cards.get(index);
+        // FOR IF ARRAY HAS NO REVEALED cards
+        Card i = cards.get(index);
         if (i.getState() == HIDDEN) {
             i.setState(REVEALED);
             return;
+        }*/
+    }
+
+
+    /**
+     * Sets the card at index to HIDDEN if not PAIRED
+     * @param index of card in cards
+     */
+    void flipDown(int index){
+        Card card = cards.get(index);
+        if (card.getState() != PAIRED){
+            card.setState(HIDDEN);
         }
     }
 
-    public boolean isGameOver() {
-        Iterator<Card> itr = Cards.iterator();
-        while (itr.hasNext()) {
-            Card i = itr.next();
+
+    /**
+     * @return false if any of the cards are not PAIRED, returns true otherwise
+     */
+    boolean isGameOver() {
+        for (Card i : cards) {
             if (i.getState() != PAIRED) {
                 return false;
             }
@@ -74,27 +102,35 @@ public class MemoryGame {
         return true;
     }
 
-    public boolean isLegalMove(Card card) {
-        if (Cards.contains(card)) {
-            if (card.getState() == REVEALED || card.getState() == PAIRED) {
-                return false;
-            }
+    /**
+     * @param card to check
+     * @return false if card is not HIDDEN, returns true otherwise
+     */
+    private boolean isLegalMove(Card card) {
+        if (cards.contains(card)) {
+            return card.getState() == HIDDEN;
         }
         return true;
     }
 
-    public ArrayList<Card> getCards(){
-        return Cards;
-    }
-
-    //format of output, [index]:[cardValue]:[cardState]
+    /**
+     * Format of output: [index]:[cardValue]:[cardState]
+     * Used for testing, prints all the information of the cards in the game
+     */
     public void printCards() {
-        Iterator<Card> itr = Cards.iterator();
+        Iterator<Card> itr = cards.iterator();
         int index = 0;
         while (itr.hasNext()) {
             Card theCard = itr.next();
             System.out.format("%-3d : %-3s : %-3S \n", index, theCard.getValue(), theCard.getState());
             index++;
         }
+    }
+
+    /**
+     * @return the cards
+     */
+    ArrayList<Card> getCards() {
+        return cards;
     }
 }
