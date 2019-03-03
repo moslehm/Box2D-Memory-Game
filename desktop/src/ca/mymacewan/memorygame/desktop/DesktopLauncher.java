f9799f46e6e4cf02df1dac9a54eb25126144c9be
Mohammad <moslehm@mymacewan.ca>
2019-03-03 00:40:33 -0700

    Modify game resolution to match the touch table's resolution

--- desktop/src/ca/mymacewan/memorygame/desktop/DesktopLauncher.java
+++ desktop/src/ca/mymacewan/memorygame/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import ca.mymacewan.memorygame.MemoryGameView;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1366;
-		config.height = 768;
+		config.width = 3840;
+		config.height = 2160;
 		new LwjglApplication(new MemoryGameView(), config);
 	}
 }
