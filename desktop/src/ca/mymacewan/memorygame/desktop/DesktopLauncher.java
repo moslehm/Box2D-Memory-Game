e57d31b077187631ed0ab23e6861e50c5c6ab3be
Mohammad <moslehm@mymacewan.ca>
2019-04-02 20:50:05 -0600

    Enable fullscreen mode for play testing

--- desktop/src/ca/mymacewan/memorygame/desktop/DesktopLauncher.java
+++ desktop/src/ca/mymacewan/memorygame/desktop/DesktopLauncher.java
@@ -9,6 +9,7 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1920; // Table: 3840 Tablet: 2048 Laptop: 1366
 		config.height = 1080; // Table: 2160 Tablet: 1536 Laptop: 768
+		config.fullscreen = true;
 		config.samples = 4; //or 2 or 8 or 16
 		new LwjglApplication(new MemoryGameView(), config);
 	}
